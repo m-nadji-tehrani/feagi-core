62177dbaa865e12783c9cda3fbcc5d053343fc16
Mohammad Nadji Tehrani <mntehrani80@gmail.com>
2018-12-27 16:26:06 -0600

    Visualizer

--- vis_2.2.py
+++ vis_2.2.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-This standalone module is intended to launch independent of the main brain application with the purpose of reading the
-contents of connectome and visualizing various aspects.
+    Animated 3D sinc function
 """
 
 from pyqtgraph.Qt import QtCore, QtGui
 import pyqtgraph.opengl as gl
 import pyqtgraph as pg
+import numpy as np
+import sys
 
 import os
 import json
@@ -71,52 +72,57 @@ class Visualizer(object):
         gz.translate(0, 0, -10)
         self.w.addItem(gz)
 
-        # self.n = 1
-        # self.m = 1000
-        # self.y = np.linspace(-10, 10, self.n)
-        # self.x = np.linspace(-10, 10, self.m)
+        # self.n = 2
+        # self.m = 3
+        # self.y = np.linspace(0, 10, self.n)
+        # self.x = np.linspace(0, 10, self.m)
         # self.phase = 0
-        #
-        # for i in range(self.n):
-        #     yi = np.array([self.y[i]] * self.m)
-        #     d = np.sqrt(self.x ** 2 + yi ** 2)
-        #     z = 10 * np.cos(d + self.phase) / (d + 1)
-        #     pts = np.vstack([self.x, yi, z]).transpose()
-        #     self.traces[i] = gl.GLLinePlotItem(pos=pts, color=pg.glColor(
-        #         (i, self.n * 1.3)), width=(i + 1) / 10, antialias=True)
-        #     self.w.addItem(self.traces[i])
+
+        pts = np.array([[-1, -1, -1], [5, 5, 5]])
+
+        for i in range(pts.shape[0]):
+            # yi = np.array([self.y[i]] * self.m)
+            # d = np.sqrt(self.x ** 2 + yi ** 2)
+            # z = 10 * np.cos(d + self.phase) / (d + 1)
+            # pts = np.vstack([self.x, yi, z]).transpose()
+
+            print('***', pts.shape)
+            self.traces[i] = gl.GLScatterPlotItem(pos=pts,
+                                                  size=np.array([10,30]),
+                                                  color=np.array([[0.11,0.7, 1, 1],
+                                                                  [0.5, 0.5, 0.5, 0.5]]),
+                                                  pxMode=True)
+            self.w.addItem(self.traces[i])
+            print('\n', self.traces[i])
+
+
+
 
     def start(self):
         if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
             QtGui.QApplication.instance().exec_()
 
-    def set_plotdata(self, name, points, color, width):
-        self.traces[name].setData(pos=points, color=color, width=width)
-
-    # def update(self, pos):
+    # def set_plotdata(self, name, points, color, width):
+    #     self.traces[name].setData(pos=points, color=color, width=width)
+    #
+    # def update(self):
     #     for i in range(self.n):
-    #         # yi = np.array([self.y[i]] * self.m)
-    #         # d = np.sqrt(self.x ** 2 + yi ** 2)
-    #         # z = 10 * np.cos(d + self.phase) / (d + 1)
-    #         # pts = np.vstack([self.x, yi, z]).transpose()
+    #         yi = np.array([self.y[i]] * self.m)
+    #         d = np.sqrt(self.x ** 2 + yi ** 2)
+    #         z = 10 * np.cos(d + self.phase) / (d + 1)
+    #         pts = np.vstack([self.x, yi, z]).transpose()
     #         self.set_plotdata(
-    #             name=i, points=pos,
+    #             name=i, points=pts,
     #             color=pg.glColor((i, self.n * 1.3)),
     #             width=(i + 1) / 10
     #         )
     #         self.phase -= .003
 
-    def animation(self, pos):
+    def animation(self):
         timer = QtCore.QTimer()
-        timer.timeout.connect(self.set_plotdata(
-                name=0, points=pos,
-                color=pg.glColor((1, 1.3)),
-                width=0.1
-            ))
-        timer.start(20)
+        # timer.timeout.connect(self.update)
+        timer.start(100)
         self.start()
-
-
 
 def connectome_visualizer(cortical_area, neuron_show=False, neighbor_show=False, threshold=0):
     """Visualizes the Neurons in the connectome"""
@@ -188,14 +194,9 @@ def connectome_visualizer(cortical_area, neuron_show=False, neighbor_show=False,
         time.sleep(5)
 
 
-
-
+# Start Qt event loop unless running in interactive mode.
 if __name__ == '__main__':
     v = Visualizer()
-    # connectome_visualizer(sys.argv[2], neuron_show=False, neighbor_show=True)
-    connectome_visualizer('vision_memory', neighbor_show=True, neuron_show=False)
+    v.animation()
 
-    # connectome_visualizer_xxx(sys.argv[2], sys.argv[3], neuron_show=False, neighbor_show=True)
-
-    # if sys.flags.interactive != 1:
-    #     app.run()
+    # connectome_visualizer('vision_memory', neighbor_show=True, neuron_show=False)

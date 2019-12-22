aee27d283dce84a6a1dfc064d824f9b4d9acc477
Mohammad Nadji-Tehrani <m.nadji.tehrani@gmail.com>
2019-12-21 19:36:26 -0600

    path adjustments, requirements.txt

--- cython_libs/cython_setup.py
+++ cython_libs/cython_setup.py
@@ -11,4 +11,4 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(ext_modules=cythonize("neuron_functions_cy.pyx"))
-setup(ext_modules=cythonize("ipu_vision_cy.pyx"))
+# setup(ext_modules=cythonize("ipu_vision_cy.pyx"))

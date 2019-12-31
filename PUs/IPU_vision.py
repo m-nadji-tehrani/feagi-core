45dfc01e72ece61d5618629321804bff9c7130f4
Mohammad Nadji-Tehrani <m.nadji.tehrani@gmail.com>
2019-12-31 14:38:31 -0600

    Fix for MNIST path issue

--- PUs/IPU_vision.py
+++ PUs/IPU_vision.py
@@ -147,21 +147,22 @@ class MNIST:
         """
 
         # database = runtime_data.parameters["InitData"]["image_database"]
-
-        path = "../../" + database + "/"
-        path2 = "../" + database + "/"
+        current_path = os.path.dirname(os.path.abspath(__file__))
+        print(">> >> >> Current folder is:", current_path)
+        path = current_path + "/../../../" + database + "/"
+        # path2 = "../" + database + "/"
 
         if dataset is "training":
                 fname_img = os.path.join(path, 'train-images.idx3-ubyte')
                 fname_lbl = os.path.join(path, 'train-labels.idx1-ubyte')
-                fname_img2 = os.path.join(path2, 'train-images.idx3-ubyte')
-                fname_lbl2 = os.path.join(path2, 'train-labels.idx1-ubyte')
+                # fname_img2 = os.path.join(path2, 'train-images.idx3-ubyte')
+                # fname_lbl2 = os.path.join(path2, 'train-labels.idx1-ubyte')
 
         elif dataset is "testing":
             fname_img = os.path.join(path, 't10k-images.idx3-ubyte')
             fname_lbl = os.path.join(path, 't10k-labels.idx1-ubyte')
-            fname_img2 = os.path.join(path2, 't10k-images.idx3-ubyte')
-            fname_lbl2 = os.path.join(path2, 't10k-labels.idx1-ubyte')
+            # fname_img2 = os.path.join(path2, 't10k-images.idx3-ubyte')
+            # fname_lbl2 = os.path.join(path2, 't10k-labels.idx1-ubyte')
         else:
             raise Exception(ValueError, "data set must be 'testing' or 'training'")
 
@@ -175,13 +176,14 @@ class MNIST:
                 magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
                 img = np.fromfile(fimg, dtype=np.uint8).reshape(len(lbl), rows, cols)
         except FileNotFoundError:
-            with open(fname_lbl2, 'rb') as flbl:
-                magic, num = struct.unpack(">II", flbl.read(8))
-                lbl = np.fromfile(flbl, dtype=np.int8)
-
-            with open(fname_img2, 'rb') as fimg:
-                magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
-                img = np.fromfile(fimg, dtype=np.uint8).reshape(len(lbl), rows, cols)
+            print("Error: Could not find raw resources!")
+            # with open(fname_lbl2, 'rb') as flbl:
+            #     magic, num = struct.unpack(">II", flbl.read(8))
+            #     lbl = np.fromfile(flbl, dtype=np.int8)
+            #
+            # with open(fname_img2, 'rb') as fimg:
+            #     magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
+            #     img = np.fromfile(fimg, dtype=np.uint8).reshape(len(lbl), rows, cols)
 
         get_img = lambda idx: (lbl[idx], img[idx])
 

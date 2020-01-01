572972214bbe27128412f6ec629cb470114a0da6
Mohammad Nadji-Tehrani <m.nadji.tehrani@gmail.com>
2020-01-01 15:54:26 -0600

    path

--- PUs/IPU_vision.py
+++ PUs/IPU_vision.py
@@ -149,7 +149,8 @@ class MNIST:
         # database = runtime_data.parameters["InitData"]["image_database"]
         current_path = os.path.dirname(os.path.abspath(__file__))
         print(">> >> **** >> Current folder is:", current_path)
-        path = current_path + "/../raw/" + database + "/"
+        # path = current_path + "/../raw/" + database + "/"
+        path = "/opt/source-code/feagi-core/raw/MNIST/"
         # path2 = "../" + database + "/"
 
         if dataset is "training":
